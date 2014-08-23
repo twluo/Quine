5ed501914997c29c3cb90ea58badc4ecc9cd1d93
Kirk <kkw013@ucsd.edu>
2014-08-23 15:54:31 -0700

    Created test file outlines

---
+++ 140Lab4/src/BooleanExpressionTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BooleanExpressionTest {
+
+	@Test
+	public void testGetImplicantList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoTabulationMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoQuineMcCluskey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoPetricksMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenVerilog() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 140Lab4/src/ImplicantTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ImplicantTest {
+
+	@Test
+	public void testGetMSB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLSB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumVars() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinterms() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImplicantLongLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImplicantLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsImplicant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVerilogExpression() {
+		fail("Not yet implemented");
+	}
+
+}
